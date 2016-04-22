79fdd60ba9f0cbe002921a25b7bbdee00e5d5808
SH15BG0110 <2425113680@qq.com>
2016-04-22 10:07:46 +0800

    Initial Commit

---
+++ GiantWeibo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GiantWeibo
+//
+//  Created by SH15BG0110 on 16/4/22.
+//  Copyright © 2016年 SH15BG0110. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
