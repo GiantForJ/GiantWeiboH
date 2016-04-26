096c1a02c6f38a8f3d80a2accf9e216ae8557bad
zgy <sh15bg0110@zgy.local>
2016-04-26 16:24:27 +0800

    1.重新修改文件，可实现登录记录，

--- GiantWeibo/Classes/OAuth/OAuthViewController.swift
+++ GiantWeibo/Classes/OAuth/OAuthViewController.swift
@@ -1,144 +1,126 @@
 //
 //  OAuthViewController.swift
-//  GiantWeibo
+//  DSWeibo
 //
-//  Created by SH15BG0110 on 16/4/26.
-//  Copyright © 2016年 SH15BG0110. All rights reserved.
+//  Created by xiaomage on 15/9/10.
+//  Copyright © 2015年 小码哥. All rights reserved.
 //
 
 import UIKit
 import SVProgressHUD
 
+
 class OAuthViewController: UIViewController {
 
-    let WB_App_KEY = "23154210"
+    let WB_App_Key = "23154210"
     let WB_App_Secret = "8a156454204d15c2fb375d92c7c47153"
-    let WB_redirect_url = "http://www.520it.com"
+    let WB_redirect_uri = "http://www.520it.com"
     
-    
-    
-    /**
-     替换View为webView
-     */
     override func loadView() {
         view = webView
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //0.初始化导航条
+        
+        // 0.初始化导航条
         navigationItem.title = "帅猪猪微博"
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "关闭", style: UIBarButtonItemStyle.Plain, target: self, action: "close")
         
-        //1.获取未授权的RequestToken
-        let urlStr = "https://api.weibo.com/oauth2/authorize?client_id=\(WB_App_KEY)&redirect_uri=\(WB_redirect_url)"
-        
+        // 1.获取未授权的RequestToken
+        // 要求SSL1.2
+        let urlStr = "https://api.weibo.com/oauth2/authorize?client_id=\(WB_App_Key)&redirect_uri=\(WB_redirect_uri)"
         let url = NSURL(string: urlStr)
         let request = NSURLRequest(URL: url!)
-        
         webView.loadRequest(request)
-        
     }
     
-    func close()  {
+    func close()
+    {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
-    //MARK: - 懒加载
-    private lazy var webView:UIWebView = {
+    // MARK: - 懒加载
+    private lazy var webView: UIWebView = {
         let wv = UIWebView()
         wv.delegate = self
         return wv
     }()
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
-extension OAuthViewController:UIWebViewDelegate{
-    //如果返回true正常加载  返回false不加载
-    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
-        //加载授权界面
-        //https://api.weibo.com/oauth2/authorize?client_id=23154210&redirect_uri=http://www.520it.com
-        //授权成功
-//        Optional("http://www.520it.com/?code=1ede87f6eed04a6548898be4a63093bb")
-        //取消授权
-//        "http://www.520it.com/?error_uri=%2Foauth2%2Fauthorize&error=access_denied&error_description=user%20denied%20your%20request.&error_code=21330")
-//        print(request.URL?.absoluteString)
+extension OAuthViewController: UIWebViewDelegate
+{
+    // 返回ture正常加载 , 返回false不加载
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool
+    {
+        print(request.URL?.absoluteString)
         
-        //1.判断是否是授权回调页面，如果不是就继续加载
+        // 1.判断是否是授权回调页面, 如果不是就继续加载
         let urlStr = request.URL!.absoluteString
-        
-        if !(request.URL!.absoluteString.hasPrefix(WB_redirect_url)) {
-            //继续加载
+        if !urlStr.hasPrefix(WB_redirect_uri)
+        {
+            // 继续加载
             return true
         }
         
-        //2.判断是否授权成功
+        // 2.判断是否授权成功
         let codeStr = "code="
-        //取
-        if request.URL!.query!.hasPrefix(codeStr) {
-            //授权成功
-            print("授权成功")
-            //1.取出已经授权的RequestToken
-            //(codeStr.endIndex) 是从codeStr之后的位置取字符串
-       let code =   request.URL!.query?.substringFromIndex(codeStr.endIndex)
-            print(code)
+        if request.URL!.query!.hasPrefix(codeStr)
+        {
+            // 授权成功
+            // 1.取出已经授权的RequestToken
+            let code = request.URL!.query?.substringFromIndex(codeStr.endIndex)
             
-            //2.利用已经授权的RequestToken换区accessToken
+            // 2.利用已经授权的RequestToken换取AccessToken
             loadAccessToken(code!)
-        } else {
-            //取消授权
-            print("取消授权")
+        }else
+        {
+            // 取消授权
+            // 关闭界面
             close()
         }
-        
+
         return false
     }
     
     func webViewDidStartLoad(webView: UIWebView) {
-        //提示用户正在加载
-        SVProgressHUD.showInfoWithStatus("正在加载", maskType: SVProgressHUDMaskType.Black)
-        
+        // 提示用户正在加载
+        SVProgressHUD.showInfoWithStatus("正在加载...", maskType: SVProgressHUDMaskType.Black)
     }
     
     func webViewDidFinishLoad(webView: UIWebView) {
+        // 关闭提示
         SVProgressHUD.dismiss()
     }
+    
     /**
-     换区accessToken
-    - parameter code: 已经授权的accessToken
-     */
-    private func loadAccessToken(code: String){
-        //1.定义路径
+    换取AccessToken
+    
+    :param: code 已经授权的RequestToken
+    */
+    private func loadAccessToken(code: String)
+    {
+        // 1.定义路径
         let path = "oauth2/access_token"
-        //2.封装参数
-        let params = ["client_id":WB_App_KEY,"client_secret":WB_App_Secret,"grant_type":"authorization_code","code":code,"redirect_uri":WB_redirect_url]
-        //3.发送POST请求
-        NetworkTools.shareNetworkTools().POST(path, parameters: params, success: { (_, JSON) in
-         //JSON转Data
-//            let data = NSJSONSerialization.dataWithJSONObject(JSON, options: NSJSONWritingOptions.PrettyPrinted)
-//            let str = NSString(data: data, encoding: NSUTF8StringEncoding)
-            print(JSON)
-            //同一用户对同一个应用程序授权多次access_token是一样的 有过期时间
-            //"access_token" = "2.004lDN3G0ATJZB63f16e6bc2Agn12C";
-            //字典转模型
+        // 2.封装参数
+        let params = ["client_id":WB_App_Key, "client_secret":WB_App_Secret, "grant_type":"authorization_code", "code":code, "redirect_uri":WB_redirect_uri]
+        // 3.发送POST请求
+        NetworkTools.shareNetworkTools().POST(path, parameters: params, success: { (_, JSON) -> Void in
+            // 1.字典转模型
             /*
-             plist:只能存储系统自带的数据类型
-             将对象转换为json之后写入文件中
-             */
-            let account:UserAccount = UserAccount(dict: JSON as! [String:AnyObject])
+            plist : 特点只能存储系统自带的数据类型
+            将对象转换为json之后写入文件中 --> 在公司中已经开始使用
+            偏好设置: 本质plist
+            归档 : 可以存储自定义对象
+            数据库: 用于存储大数据 , 特点效率较高
+            */
+            let account = UserAccount(dict: JSON as! [String : AnyObject])
             
-            //2.归档模型
+            // 2.归档模型
             account.saveAccount()
-           
-            }) { (_, error) in
+            
+            }) { (_, error) -> Void in
                 print(error)
         }
-        
-        
     }
-    
 }
--- GiantWeibo/Classes/OAuth/UserAccount.swift
+++ GiantWeibo/Classes/OAuth/UserAccount.swift
@@ -1,84 +1,115 @@
 //
 //  UserAccount.swift
-//  GiantWeibo
+//  DSWeibo
 //
-//  Created by SH15BG0110 on 16/4/26.
-//  Copyright © 2016年 SH15BG0110. All rights reserved.
+//  Created by xiaomage on 15/9/10.
+//  Copyright © 2015年 小码哥. All rights reserved.
 //
 
 import UIKit
 
-//Swift 2.0 打印对象需要重写CustomStringConvertible..协议中的description
-class UserAccount: NSObject ,NSCoding{
- 
-    var access_token:String?
-    var expires_in: NSNumber?
-    var uid:String?
-    override init() {
-        
+// Swift2.0 打印对象需要重写CustomStringConvertible协议中的description
+class UserAccount: NSObject , NSCoding{
+ /// 用于调用access_token，接口获取授权后的access token。
+    var access_token: String?
+ /// access_token的生命周期，单位是秒数。
+    var expires_in: NSNumber?{
+        didSet{
+            // 根据过期的秒数, 生成真正地过期时间
+            expires_Date = NSDate(timeIntervalSinceNow: expires_in!.doubleValue)
+            print(expires_Date)
+        }
     }
+    
+/// 保存用户过期时间
+    var expires_Date: NSDate?
+ /// 当前授权用户的UID。
+    var uid:String?
+    
+
     init(dict: [String: AnyObject])
     {
+        super.init()
+        /*
         access_token = dict["access_token"] as? String
+        // 注意: 如果直接赋值, 不会调用didSet
         expires_in = dict["expires_in"] as? NSNumber
         uid = dict["uid"] as? String
-        
+        */
+
+        setValuesForKeysWithDictionary(dict)
+    }
+    override func setValue(value: AnyObject?, forUndefinedKey key: String) {
+        print(key)
     }
     
     override var description: String{
-        //1.定义属性数组
-        let properties = ["access_token","expires_in","uid" ]
-        //1.根据属性数组，将属性转换为字典
-      let dict =  self.dictionaryWithValuesForKeys(properties)
-        //3.
+        // 1.定义属性数组
+        let properties = ["access_token", "expires_in", "uid"]
+        // 2.根据属性数组, 将属性转换为字典
+        let dict =  self.dictionaryWithValuesForKeys(properties)
+        // 3.将字典转换为字符串
         return "\(dict)"
     }
     
-   class  func userLogin() ->Bool{
+    /**
+    返回用户是否登录
+    */
+    class func userLogin() -> Bool
+    {
         return UserAccount.loadAccount() != nil
     }
     
-    //MARK: - 保存和读取 keyed
+    // MARK: - 保存和读取  Keyed
     /**
-     保存授权模型
-     */
-    func saveAccount(){
-    let path = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.CachesDirectory, NSSearchPathDomainMask.UserDomainMask, true).last!
-    let filePath = (path as NSString).stringByAppendingPathComponent("account.plist")
-        print("filePath\(filePath)")
-        NSKeyedArchiver.archiveRootObject(self, toFile: filePath)
-        }
-    /**
-     加载授权模型
-     */
+    保存授权模型
+    */
+    
+//    let filePath2 = (NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.CachesDirectory, NSSearchPathDomainMask.UserDomainMask, true).last! as NSString).stringByAppendingPathComponent("account.plist")
+    func saveAccount()
+    {
+        NSKeyedArchiver.archiveRootObject(self, toFile: "account.plist".cacheDir())
+    }
+    
+    /// 加载授权模型
     static var account: UserAccount?
-    class func loadAccount() ->UserAccount?{
-        //1.判断是否已经加载过
-        if account != nil {
+    class func loadAccount() -> UserAccount? {
+
+        // 1.判断是否已经加载过
+        if account != nil
+        {
             return account
         }
-        //1.加载授权模型
-        let path = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.CachesDirectory, NSSearchPathDomainMask.UserDomainMask, true).last!
-        let filePath = (path as NSString).stringByAppendingPathComponent("account.plist")
-        print("filePath \(filePath)")
-       account =  NSKeyedUnarchiver.unarchiveObjectWithFile(filePath) as? UserAccount
-        print(account)
+        // 2.加载授权模型
+        account =  NSKeyedUnarchiver.unarchiveObjectWithFile("account.plist".cacheDir()) as? UserAccount
+        
+        // 3.判断授权信息是否过期
+        // 2020-09-08 03:49:39                       2020-09-09 03:49:39
+        if account?.expires_Date?.compare(NSDate()) == NSComparisonResult.OrderedAscending
+        {
+            // 已经过期
+            return nil
+        }
+        
         return account
     }
     
-    //MARK: -NSCoding
-    //将对象写入文件中
-    func encodeWithCoder(aCoder: NSCoder) {
-        aCoder.encodeObject(access_token,forKey: "access_token")
-        aCoder.encodeObject(expires_in,forKey: "expires_in")
-        aCoder.encodeObject(uid,forKey: "uid")
-    }
     
-    //从文件中读取对象
-     required init?(coder aDecoder: NSCoder) {
+    // MARK: - NSCoding
+    // 将对象写入到文件中
+    func encodeWithCoder(aCoder: NSCoder)
+    {
+        aCoder.encodeObject(access_token, forKey: "access_token")
+        aCoder.encodeObject(expires_in, forKey: "expires_in")
+        aCoder.encodeObject(uid, forKey: "uid")
+        aCoder.encodeObject(expires_Date, forKey: "expires_Date")
+    }
+    // 从文件中读取对象
+    required init?(coder aDecoder: NSCoder)
+    {
         access_token = aDecoder.decodeObjectForKey("access_token") as? String
         expires_in = aDecoder.decodeObjectForKey("expires_in") as? NSNumber
         uid = aDecoder.decodeObjectForKey("uid") as? String
+        expires_Date = aDecoder.decodeObjectForKey("expires_Date") as? NSDate
     }
-    
 }
